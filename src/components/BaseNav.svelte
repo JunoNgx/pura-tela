315cdda0e57bd0409bb5d995dc6410f5612b335b
Juno Nguyen <juno.ngx@gmail.com>
2025-01-15 21:51:02 +0800

    feat: add css for nav

--- src/components/BaseNav.svelte
+++ src/components/BaseNav.svelte
@@ -1,5 +1,20 @@
 <nav>
-    <a href="/">Studio</a>
-    <a href="/gallery">Gallery</a>
-    <a href="/sizes">Sizes</a>
+    <a class="nav-item" href="/">Studio</a>
+    <a class="nav-item" href="/gallery">Gallery</a>
+    <a class="nav-item" href="/sizes">Sizes</a>
 </nav>
+
+<style>
+    nav {
+        width: 100%;
+        display: flex;
+        flex-direction: row;
+        justify-content: space-evenly;
+    }
+
+    .nav-item {
+        display: block;
+        text-align: center;
+        flex-grow: 10;
+    }
+</style>
