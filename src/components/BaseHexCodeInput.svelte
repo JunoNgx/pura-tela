83f3fb151185e5e30c09b5d0b0e2249de32e17d1
Juno Nguyen <juno.ngx@gmail.com>
2025-01-16 20:50:31 +0800

    feat: break out BaseHexCodeInput

---
+++ src/components/BaseHexCodeInput.svelte
@@ -0,0 +1,69 @@
+<section class="ColourInput">
+
+    <h3>Colour</h3>
+
+    <div class="ColourInput__Top">
+        <div class="ColourInput__Hex">
+            <label for="hexCode">#</label>
+            <input
+                name="hexCode"
+                type="text"
+                minlength="3"
+                maxlength="6"
+            />
+        </div>
+        <button>pick</button>
+    </div>
+
+    <div class="ColourInput__Rgb">
+        <div class="ColourInput__RgbItem">
+            <label for="red">R</label>
+            <input
+                name="red"
+                type="number"
+                min="0"
+                max="255"
+            />
+        </div>
+        <div class="ColourInput__Rgb">
+            <label for="green">G</label>
+            <input
+                name="red"
+                type="number"
+                min="0"
+                max="255"
+            />
+        </div>
+        <div class="ColourInput__Rgb">
+            <label for="blue">B</label>
+            <input
+                name="blue"
+                type="number"
+                min="0"
+                max="255"
+            />
+        </div>
+    </div>
+
+    <div class="ColourInput__Buttons">
+        <button>randomise</button>
+        <button>save colour</button>
+    </div>
+
+</section>
+
+<style>
+    .ColourInput__Top,
+    .ColourInput__Hex,
+    .ColourInput__Rgb {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        align-items: center;
+    }
+
+    .ColourInput__Top {
+        justify-content: flex-start;
+        gap: 15px;
+    }
+</style>
--- src/components/BaseStudio.svelte
+++ src/components/BaseStudio.svelte
@@ -1,53 +1,14 @@
+<script lang="ts">
+    import BaseHexCodeInput from "./BaseHexCodeInput.svelte";
+</script>
+
 <div class="studio">
     <div class="studio__canvas-container">
         <canvas id="preview-canvas"></canvas>
     </div>
     <div class="studio__control">
         <div class="studio__colour">
-            <div class="studio__hex-code">
-                <div class="studio__hex-code-input">
-                    <label for="hexCode">#</label>
-                    <input
-                        name="hexCode"
-                        type="text"
-                        minlength="3"
-                        maxlength="6"
-                    />
-                </div>
-                <button>pick</button>
-                <button>choose</button>
-                <button>randomise</button>
-            </div>
-            <div class="studio__rgb">
-                <div class="studio__rgb-item">
-                    <label for="red">R</label>
-                    <input
-                        name="red"
-                        type="number"
-                        min="0"
-                        max="255"
-                    />
-                </div>
-                <div class="studio__rgb-item">
-                    <label for="green">G</label>
-                    <input
-                        name="red"
-                        type="number"
-                        min="0"
-                        max="255"
-                    />
-                </div>
-                <div class="studio__rgb-item">
-                    <label for="blue">B</label>
-                    <input
-                        name="blue"
-                        type="number"
-                        min="0"
-                        max="255"
-                    />
-                </div>
-            </div>
-
+            <BaseHexCodeInput/>
         </div>
         <div class="studio__size">
             <h3>
@@ -86,19 +47,5 @@
 
     .studio__colour {
 
-    }
-
-    .studio__hex-code {
-        display: flex;
-        flex-direction: row;
-        justify-content: space-between;
-        align-items: center;
-    }
-
-    .studio__rgb {
-        display: flex;
-        flex-direction: row;
-        justify-content: space-between;
-        align-items: center;
     }
 </style>
