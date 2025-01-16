f2ce4ce5bf1fcab2dd60ee61f3d62125980641aa
Juno Nguyen <juno.ngx@gmail.com>
2025-01-16 23:14:50 +0800

    fix: incorrect logic to clamp 8 bit

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -32,7 +32,7 @@ export const parseRgbChannelValue = (str: string): number => {
 
 export const convertRgbToHex = (rgb: RgbColour): string => {
     const toHex = (number: number): string => {
-        const clampedValue = Math.min(0, Math.max(number, 255));
+        const clampedValue = clamp8bit(number);
         const hexStr = clampedValue.toString(16);
         const procesedHexStr = hexStr.length === 1
             ? `0${hexStr}`
