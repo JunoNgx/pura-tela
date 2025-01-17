1ac84235d0448469f47b367a92ae1ab984f98401
Juno Nguyen <juno.ngx@gmail.com>
2025-01-18 01:17:02 +0800

    feat: implement currHexCode sync with localStorage

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -6,6 +6,7 @@ import defaultColourGallery from "src/data/colours.json" with { type: "json"};
 import defaultSizeOptions from "src/data/sizes.json" with { type: "json"};
 
 import { convertHexToRgb, loadFromLocalStorage } from "./utils.js";
+import { browser } from "$app/environment";
 
 export const themeMode = writable(loadFromLocalStorage({
     key: "themeMode",
@@ -35,6 +36,15 @@ export const currHexCode = writable(loadFromLocalStorage({
     key: "currHexCode",
     defaultValue: firstHexCode,
 }));
+currHexCode.subscribe(value => {
+    if (typeof window !== 'undefined') {
+        if (!browser) {
+            return;
+        }
+
+        localStorage.setItem("currHexCode", value);
+    }
+});
 
 export const currRgbColour: Writable<RgbColour> = writable(
     convertHexToRgb(firstHexCode));
