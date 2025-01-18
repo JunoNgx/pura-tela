9391338a4e5f2caa79326926a0430b40502054d0
Juno Nguyen <juno.ngx@gmail.com>
2025-01-18 11:33:48 +0800

    feat: implement canvas render and download logic

---
+++ src/lib/canvas.ts
@@ -0,0 +1,45 @@
+export const generateImage = (
+    {width, height, bgHex, filename}:
+    {
+        width: number,
+        height: number,
+        bgHex: string,
+        filename: string,
+    }
+) => {
+    const CANVAS_ID = "renderCanvas"
+
+    const canvas = document.createElement("canvas");
+    canvas.setAttribute("id", CANVAS_ID);
+    canvas.width = width;
+    canvas.height = height;
+
+    const ctx = canvas.getContext("2d");
+    if (!ctx) {
+        return;
+    }
+
+    ctx.fillStyle = `#${bgHex}`;
+    ctx.fillRect(0, 0, width, height);
+
+    canvas.toBlob((blob) => {
+        if (blob === null) {
+            return;
+        }
+
+        const url = URL.createObjectURL(blob);
+
+        const anchorEl = document.createElement("a");
+        anchorEl.href = url;
+        anchorEl.download = filename;
+
+        document.body.appendChild(anchorEl);
+        anchorEl.click();
+        document.body.removeChild(anchorEl);
+
+
+        setTimeout(() => {
+            URL.revokeObjectURL(url);
+        })
+    });
+};
