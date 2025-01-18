bfde564f40d322d845560f2ae6932ef47311c400
Juno Nguyen <juno.ngx@gmail.com>
2025-01-18 18:21:26 +0800

    feat: implement colour list display

---
+++ src/components/BaseColourList.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+    import { colourGallery } from "src/lib/states.svelte.js";
+</script>
+
+<ul class="ColourList">
+    {#each colourGallery.val as colourItem}
+        <li class="ColourItem">
+            <div class="ColourItem__Preview"
+                style={`background-color: #${colourItem.hexCode};`}
+            ></div>
+            <div class="ColourItem__Name">
+                {colourItem.name}
+            </div>
+            <div class="ColourItem__HexCode">
+                {`#${colourItem.hexCode}`}
+            </div>
+            <div class="ColourItem__Buttons">
+                <button>pick</button>
+                <button>delete</button>
+            </div>
+        </li>
+    {/each}
+</ul>
+
+<style>
+    .ColourList {
+        display: flex;
+        flex-wrap: wrap;
+        gap: 15px;
+    }
+
+    .ColourItem {
+        list-style: none;
+        border: 1px solid #000;
+        width: 20%;
+        padding: 4px;
+    }
+
+    .ColourItem__Preview {
+        aspect-ratio: 1/1;
+        width: 100%;
+    }
+
+    .ColourItem__Buttons {
+        display: flex;
+        flex-direction: row;
+        justify-content: flex-end;
+    }
+</style>
--- src/routes/gallery/+page.svelte
+++ src/routes/gallery/+page.svelte
@@ -1,3 +1,7 @@
+<script lang="ts">
+    import BaseColourList from "src/components/BaseColourList.svelte";
+</script>
+
 <h1>Gallery</h1>
 
-<p>List of colours</p>
+<BaseColourList/>
