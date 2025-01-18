6e17c7004282ee3b020de3a8b31e61745d6e57d5
Juno Nguyen <juno.ngx@gmail.com>
2025-01-18 15:53:39 +0800

    feat: implement update of dimensions

--- src/components/BaseDimensionsSelection.svelte
+++ src/components/BaseDimensionsSelection.svelte
@@ -1,14 +1,30 @@
-<script>
-    import { sizeOptions } from "src/lib/states.svelte";
+<script lang="ts">
+    import { currSizeOptionIndex, sizeOptions } from "src/lib/states.svelte";
+
+    const handleSizeOptionChange = (rawNewIndex: string) => {
+        let newIndex;
+
+        try {
+            newIndex = parseInt(rawNewIndex);
+        } catch (error) {
+            console.error("ERROR: invalid index value for size option")
+            return;
+        }
+
+        currSizeOptionIndex.set(newIndex);
+    }
 </script>
 
 <section class="SizeSelection">
     <h3>
         <label for="size">Dimensions</label>
     </h3>
-    <select>
-        {#each sizeOptions as sizeOptionItem}
-            <option>{sizeOptionItem.name}</option>
+    <select
+        value={currSizeOptionIndex.val}
+        on:input={(e) => handleSizeOptionChange((e.target as HTMLSelectElement).value)}
+    >
+        {#each sizeOptions as sizeOptionItem, index}
+            <option value={index}>{sizeOptionItem.name}</option>
         {/each}
     </select>
 </section>
