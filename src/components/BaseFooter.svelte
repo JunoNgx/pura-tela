499c849dfb35df30425c83e024f652166565ffdf
Juno Nguyen <juno.ngx@gmail.com>
2025-01-20 00:58:34 +0800

    feat: add footer content

--- src/components/BaseFooter.svelte
+++ src/components/BaseFooter.svelte
@@ -1,7 +1,19 @@
-<footer>Made by Juno Nguyen</footer>
+<footer>
+    <p>
+        developed by <a href="https://www.junongx.com/" target="_blank" rel="noopener noreferrer">Juno Nguyen</a>
+    </p>
+    <p>
+        designed by <a href="https://caseykwokdinata.webflow.io/" target="_blank" rel="noopener noreferrer">Casey Kwokdinata</a>
+    </p>
+</footer>
 
 <style>
     footer {
         text-align: center;
+        margin-bottom: 1.5rem;
+    }
+
+    p {
+        margin: 0.5rem 0;
     }
 </style>
