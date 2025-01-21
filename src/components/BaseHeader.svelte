424cdbda47ea5427b1d6f1e742a725279e2d9e76
Juno Nguyen <juno.ngx@gmail.com>
2025-01-21 22:24:29 +0800

    feat: implement help dialog

--- src/components/BaseHeader.svelte
+++ src/components/BaseHeader.svelte
@@ -1,10 +1,38 @@
 <script lang="ts">
+    import MaterialSymbolsLightHelpOutline from "~icons/material-symbols-light/help-outline";
     import BaseThemeModeContainer from "src/components/BaseThemeModeContainer.svelte";
+	import BaseHelpDialog from "src/components/BaseHelpDialog.svelte";
+    import { onMount } from 'svelte';
+
+
+    const getHelpDialog = () => {
+        return document.getElementById("helpDialog") as HTMLDialogElement;
+    };
+
+    const showDialog = () => {
+        const helpDialog = getHelpDialog();
+        helpDialog?.showModal();
+    };
+
+    const hideDialog = () => {
+        const helpDialog = getHelpDialog();
+        helpDialog?.close();
+    };
+
+    onMount(() => {
+        showDialog()
+    })
 </script>
 
 <header class="Header">
     <div class="Header__Left">
         <h1>Pura Tela</h1>
+        <button class="Header__HelpBtn IconButton"
+            onclick={showDialog}
+        >
+            <MaterialSymbolsLightHelpOutline/>
+        </button>
+        <BaseHelpDialog closeDialog={hideDialog}/>
     </div>
     <div class="Header_Right">
         <BaseThemeModeContainer/>
@@ -17,6 +45,16 @@
         justify-content: space-between;
         align-items: center;
         width: 100%;
+    }
+
+    .Header__Left {
+        display: flex;
+        gap: 1rem;
+        align-items: center;
+    }
+
+    .Header__HelpBtn {
+        border: none;
     }
 
     @media screen and (width < 600px) {
