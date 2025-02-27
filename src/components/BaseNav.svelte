92a6c3b71cba6df8ef1faf648c084dc78983e9ee
Juno Nguyen <juno.ngx@gmail.com>
2025-02-28 00:53:16 +0800

    #9: draft logic for mobile navbar toggling

--- src/components/BaseNav.svelte
+++ src/components/BaseNav.svelte
@@ -1,10 +1,30 @@
 <script lang="ts">
-	import NavItem from "src/components/NavItem.svelte";
+	import MaterialSymbolsLightViewSidebarOutlineSharp from "~icons/material-symbols-light/view-sidebar-outline-sharp";
+    import NavItem from "src/components/NavItem.svelte";
 	import BaseThemeModeContainer from "./BaseThemeModeContainer.svelte";
+
+    let isExpanded = $state(false);
+
+    const toggleSidebar = () => {
+        isExpanded = !isExpanded;
+    };
 </script>
 
-<div class="Nav">
-    <div class="Nav__OverlayBg"></div>
+<div class="Nav"
+    class:Nav--IsExpanded={isExpanded}
+>
+    <div class="Nav__OverlayBg"
+        role="region"
+        onclick={() => isExpanded = false}
+    ></div>
+    <div class="Nav__SwitchContainer">
+        <button class="IconButton"
+            onclick={toggleSidebar}
+        >
+            <MaterialSymbolsLightViewSidebarOutlineSharp/>
+        </button>
+    </div>
+
     <nav class="Nav__Content Card">
         <div class="Nav__LogoContainer">
             <img class="Nav__LogoIcon"
@@ -53,6 +73,15 @@
         backdrop-filter: blur(10px);
     }
 
+    .Nav__SwitchContainer {
+        display: none;
+        position: absolute;
+        bottom: 1.5rem;
+        left: 1.5rem;
+        padding: 0.5rem;
+        background-color: var(--colBrand1);
+    }
+
     .Nav__LogoContainer {
         display: flex;
         justify-content: flex-end;
@@ -86,12 +115,31 @@
 
     @media screen and (width < 850px) {
         .Nav {
-            /* position: absolute;
+            position: static;
+        }
+
+        .Nav__Content {
+            position: absolute;
             top: 0;
-            height: 100dvh; */
+            left: -100%;
+            height: 100dvh;
             /* left: -100%; */
             /* width: 75vh; */
             /* margin-top: 1rem; */
+        }
+
+        .Nav--IsExpanded {
+            .Nav__Content {
+                left: 0;
+            }
+
+            .Nav__OverlayBg {
+                display: block;
+            }
+        }
+
+        .Nav__SwitchContainer {
+            display: block;
         }
     }
 </style>
