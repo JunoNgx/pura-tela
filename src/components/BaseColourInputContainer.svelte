0e599cc2cad6b5468b23c3539d3d7cdc2aee4c8f
Juno Nguyen <juno.ngx@gmail.com>
2025-03-04 01:14:03 +0800

    #11: implement BaseColourInputContainer

---
+++ src/components/BaseColourInputContainer.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { getColoursInUse } from "src/lib/states.svelte.js";
+
+    import ColourInputItem from "src/components/ColourInputItem.svelte";
+
+    const coloursInUse = $derived(getColoursInUse());
+</script>
+
+<div class="ColourInputContainer">
+    <ul class="ColourInputContainer__MainList">
+        {#each coloursInUse as _, index}
+            <ColourInputItem index={index}/>
+        {/each}
+    </ul>
+</div>
