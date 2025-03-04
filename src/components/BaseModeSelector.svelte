5176dcc9d4a0b35fc0c48d3858d61b2faa6360aa
Juno Nguyen <juno.ngx@gmail.com>
2025-03-04 22:23:38 +0800

    #11: attach readjustCurrColourInUseCount() to wallpaper mode change logic

--- src/components/BaseModeSelector.svelte
+++ src/components/BaseModeSelector.svelte
@@ -1,19 +1,10 @@
 <script lang="ts">
     import { WallpaperMode } from "src/lib/types.js";
-    import { currWallpaperMode, getCurrColourInUseCount, getMaxColourInUseCount, getMinColourInUseCount, isGradientMode, isSolidMode, setCurrColourInUseCount } from "src/lib/states.svelte.js";
+    import { currWallpaperMode, isGradientMode, isSolidMode, readjustCurrColourInUseCount } from "src/lib/states.svelte.js";
 
     const handleWallpaperModeChange = (newValue: WallpaperMode) => {
         currWallpaperMode.set(newValue);
-
-        const currMinColourCount = getMinColourInUseCount();
-        if (getCurrColourInUseCount() < currMinColourCount) {
-            setCurrColourInUseCount(currMinColourCount);
-        }
-
-        const currMaxColourCount = getMaxColourInUseCount();
-        if (getCurrColourInUseCount() > currMaxColourCount) {
-            setCurrColourInUseCount(currMaxColourCount);
-        }
+        readjustCurrColourInUseCount();
     }
 </script>
 
