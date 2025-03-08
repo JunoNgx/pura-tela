b605f827340ace7f37303b1b75e6ea10d5053cf5
Juno Nguyen <juno.ngx@gmail.com>
2025-03-08 19:30:00 +0800

    #12: add border to paletteBox

--- src/components/PaletteListItem.svelte
+++ src/components/PaletteListItem.svelte
@@ -24,6 +24,7 @@
     .PaletteListItem__PaletteBox {
         height: 200px;
         width: 100%;
+        border: 1px solid var(--colPri);
         display: flex;
         flex-direction: row;
     }
