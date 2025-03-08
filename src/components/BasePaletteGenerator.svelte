d7adb728bc12e1ad2ea3035c5d85c9eefb9d8ad2
Juno Nguyen <juno.ngx@gmail.com>
2025-03-08 22:02:52 +0800

    #7: implement basic palette generation placeholders

---
+++ src/components/BasePaletteGenerator.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+
+</script>
+
+<div class="PaletteGenerator">
+
+</div>
+
+<style>
+    .PaletteGenerator {
+        width: 100%;
+        min-height: 25rem;
+        background-color: aquamarine;
+    }
+</style>
