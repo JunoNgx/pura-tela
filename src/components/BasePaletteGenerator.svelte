a136b11509192e1f2079e19b6f383882f11ec33d
Juno Nguyen <juno.ngx@gmail.com>
2025-03-09 01:08:36 +0800

    #7: implement generate button

--- src/components/BasePaletteGenerator.svelte
+++ src/components/BasePaletteGenerator.svelte
@@ -1,30 +1,52 @@
 <script lang="ts">
+    import MaterialSymbolsLightGesture from '~icons/material-symbols-light/gesture';
+
 	import { palGenColours } from "src/lib/states.svelte.js";
 	import PaletteGeneratorItem from "./PaletteGeneratorItem.svelte";
 
+    const generatePalette = () => {
 
+    };
 </script>
 
 <div class="PaletteGenerator">
-    {#each palGenColours.val as palGenItem, index}
-        <PaletteGeneratorItem 
-            palGenItem={palGenItem}
-            index={index}
-        />
-    {/each}
+    <div class="PaletteGenerator__PaletteBox">
+        {#each palGenColours.val as palGenItem, index}
+            <PaletteGeneratorItem 
+                palGenItem={palGenItem}
+                index={index}
+            />
+        {/each}
+    </div>
+
+    <div class="PaletteGenerator__ActionsContainer">
+        <button class="PaletteGenerator__ActionBtn IconButtonWithLabel"
+            onclick={generatePalette}
+            title={"Generate new palettes"}
+            aria-label={"Generate new palettes"}
+        >
+            <MaterialSymbolsLightGesture />
+            <span>Generate</span>
+        </button>
+    </div>
 </div>
 
 <style>
-    .PaletteGenerator {
+    .PaletteGenerator__PaletteBox {
         display: flex;
         flex-direction: row;
         width: 100%;
         min-height: 40rem;
-        background-color: aquamarine;
+    }
+
+    .PaletteGenerator__ActionsContainer {
+        margin-top: 2rem;
+        display: flex;
+        justify-content: center;
     }
 
     @media screen and (width < 850px) {
-        .PaletteGenerator {
+        .PaletteGenerator__PaletteBox {
             display: flex;
             flex-direction: column;
             min-height: 60vh;
