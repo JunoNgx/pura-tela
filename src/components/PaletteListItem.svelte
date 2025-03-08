854e81289a326dee7251c4d8ad6c57e9fe6aca85
Juno Nguyen <juno.ngx@gmail.com>
2025-03-08 16:29:40 +0800

    #8: increase transition animation time for palette display

--- src/components/PaletteListItem.svelte
+++ src/components/PaletteListItem.svelte
@@ -35,7 +35,7 @@
     .PaletteListItem__PaletteColourItem {
         flex-grow: 1;
         border: none;
-        transition: all var(--transTime) ease-in-out;
+        transition: all var(--transTimeFast) ease-in-out;
         padding: 0;
         /* Stop the content (hexCode) from restricting min-width when not focused*/
         width: 0px;
@@ -46,7 +46,7 @@
         padding: 0.25rem;
         color: var(--colWhite);
         background-color: rgba(var(--colBlackRgb), 0.5);
-        transition: all var(--transTime) ease-in-out;
+        transition: all var(--transTimeFast) ease-in-out;
     }
 
     .PaletteListItem__PaletteColourItem:focus,
