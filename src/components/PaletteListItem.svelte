1abd093c2cb54b713a828385fbd5620be0e45daf
Juno Nguyen <juno.ngx@gmail.com>
2025-03-08 17:33:41 +0800

    #8: refactor the colour btn in the palette into its own component

--- src/components/PaletteListItem.svelte
+++ src/components/PaletteListItem.svelte
@@ -1,17 +1,13 @@
 <script lang="ts">
+	import PaletteListItemColourBtn from "./PaletteListItemColourBtn.svelte";
+
     let { paletteItem } = $props();
 </script>
 
 <ul class="PaletteListItem">
     <div class="PaletteListItem__PaletteBox">
         {#each paletteItem.colours as colourItem}
-            <button class="PaletteListItem__PaletteColourItem"
-                style="{`background-color: #${colourItem}`}"
-            >
-                <span class="PaletteListItem__PaletteColourCode">
-                    {colourItem}
-                </span>
-            </button>
+            <PaletteListItemColourBtn colourItem={colourItem} />
         {/each}
     </div>
     <div class="PaletteListItem__Name">{paletteItem.name}</div>
@@ -30,32 +26,5 @@
         width: 100%;
         display: flex;
         flex-direction: row;
-    }
-
-    .PaletteListItem__PaletteColourItem {
-        flex-grow: 1;
-        border: none;
-        transition: all var(--transTimeFast) ease-in-out;
-        padding: 0;
-        /* Stop the content (hexCode) from restricting min-width when not focused*/
-        width: 0px;
-    }
-
-    .PaletteListItem__PaletteColourCode {
-        opacity: 0;
-        padding: 0.25rem;
-        color: var(--colWhite);
-        background-color: rgba(var(--colBlackRgb), 0.5);
-        transition: all var(--transTimeFast) ease-in-out;
-    }
-
-    .PaletteListItem__PaletteColourItem:focus,
-    .PaletteListItem__PaletteColourItem:hover {
-        flex-grow: 2;
-        z-index: 30;
-
-        .PaletteListItem__PaletteColourCode {
-            opacity: 1;
-        }
     }
 </style>
---
+++ src/components/PaletteListItemColourBtn.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+    let { colourItem } = $props();
+</script>
+
+<button class="PaletteListColourBtn"
+    style="{`background-color: #${colourItem}`}"
+>
+    <span class="PaletteListColourBtn__Label">
+        {colourItem}
+    </span>
+</button>
+
+<style>
+    .PaletteListColourBtn {
+        flex-grow: 1;
+        border: none;
+        transition: all var(--transTimeFast) ease-in-out;
+        padding: 0;
+        /* Stop the content (hexCode) from restricting min-width when not focused*/
+        width: 0px;
+    }
+
+    .PaletteListColourBtn__Label {
+        opacity: 0;
+        padding: 0.25rem;
+        color: var(--colWhite);
+        background-color: rgba(var(--colBlackRgb), 0.5);
+        transition: all var(--transTimeFast) ease-in-out;
+    }
+
+    .PaletteListColourBtn:focus,
+    .PaletteListColourBtn:hover {
+        flex-grow: 2;
+        z-index: 30;
+
+        .PaletteListColourBtn__Label {
+            opacity: 1;
+        }
+    }
+</style>
+
+
+
