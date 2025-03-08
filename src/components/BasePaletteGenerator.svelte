60c1c30f22d5ecf25906002bef7d27750030cc3a
Juno Nguyen <juno.ngx@gmail.com>
2025-03-08 23:00:53 +0800

    #7: implement basic palette generator item

--- src/components/BasePaletteGenerator.svelte
+++ src/components/BasePaletteGenerator.svelte
@@ -1,15 +1,33 @@
 <script lang="ts">
+	import { palGenColours } from "src/lib/states.svelte.js";
+	import PaletteGeneratorItem from "./PaletteGeneratorItem.svelte";
+
 
 </script>
 
 <div class="PaletteGenerator">
-
+    {#each palGenColours.val as palGenItem, index}
+        <PaletteGeneratorItem 
+            palGenItem={palGenItem}
+            index={index}
+        />
+    {/each}
 </div>
 
 <style>
     .PaletteGenerator {
+        display: flex;
+        flex-direction: row;
         width: 100%;
         min-height: 25rem;
         background-color: aquamarine;
+    }
+
+    @media screen and (width < 850px) {
+        .PaletteGenerator {
+            display: flex;
+            flex-direction: column;
+            min-height: 60vh;
+        }
     }
 </style>
---
+++ src/components/PaletteGeneratorItem.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import type { PalGenItem } from "src/lib/types.js";
+
+    type PaletteGeneratorItemProps = {
+        palGenItem: PalGenItem,
+        index: number,
+    };
+
+    let { palGenItem, index }: PaletteGeneratorItemProps = $props();
+
+</script>
+
+<div class="PalGenItem"
+    style={`background-color: #${palGenItem.colour}`}
+>
+
+</div>
+
+<style>
+    .PalGenItem {
+        flex-grow: 1;
+    }
+</style>
+
