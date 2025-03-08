7f336d49bda505e7b207940acbd03077dacbc911
Juno Nguyen <juno.ngx@gmail.com>
2025-03-08 23:14:04 +0800

    #7: add hexCode to PalGenItem

--- src/components/PaletteGeneratorItem.svelte
+++ src/components/PaletteGeneratorItem.svelte
@@ -13,12 +13,21 @@
 <div class="PalGenItem"
     style={`background-color: #${palGenItem.colour}`}
 >
-
+    <div class="PalGenItem__HexCode">
+        {palGenItem.colour}
+    </div>
 </div>
 
 <style>
     .PalGenItem {
+        position: relative;
         flex-grow: 1;
+    }
+
+    .PalGenItem__HexCode {
+        padding: 0.25rem;
+        background-color: rgba(var(--colBlackRgb), 0.5);
+        width: fit-content;
     }
 </style>
 
