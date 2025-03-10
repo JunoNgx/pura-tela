90e4a0d8fdf3f02fd22c4a5fb914da4920200541
Juno Nguyen <juno.ngx@gmail.com>
2025-03-11 00:20:50 +0800

    #20: draft basic wallGenColours state

---
+++ src/states/wallGenColours.svelte.ts
@@ -0,0 +1,35 @@
+// @ts-ignore
+import defaultColourGallery from "src/data/colours.json";
+import { createLocalStorageSyncedState } from "src/states/stateUtils.js";
+import { type State } from "src/lib/types.js";
+import { isHexCodeValid } from "src/lib/utils.js";
+
+const defaultCurrColoursValue = [
+    defaultColourGallery[0].hexCode,
+    defaultColourGallery[1].hexCode,
+    defaultColourGallery[2].hexCode,
+    defaultColourGallery[3].hexCode,
+    defaultColourGallery[4].hexCode,
+];
+
+export const isCurrColoursValid = (data: string[]) => {
+    if (!data) return false;
+    if (data.length !== 5) return false;
+
+    try {
+        for (const item of data) {
+            if (!item) return false;
+            if (!isHexCodeValid(item)) return false;
+        }
+
+        return true;
+    } catch (err) {
+        return false;
+    }
+};
+
+export const wallGenColours = createLocalStorageSyncedState({
+    key: "currColours",
+    defaultValue: defaultCurrColoursValue,
+    validationFunc: isCurrColoursValid,
+}) as State<string[]>;
