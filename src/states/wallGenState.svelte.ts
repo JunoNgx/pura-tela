7821e16d3ecee8ee050640b0722255a240bc4d2c
Juno Nguyen <juno.ngx@gmail.com>
2025-03-11 00:38:55 +0800

    #20: carry over other utils methods for wallGenColours

--- src/states/wallGenState.svelte.ts
+++ src/states/wallGenState.svelte.ts
@@ -2,7 +2,7 @@
 import defaultColourGallery from "src/data/colours.json";
 import { createLocalStorageSyncedState } from "src/states/stateUtils.js";
 import { type State } from "src/lib/types.js";
-import { isHexCodeValid } from "src/lib/utils.js";
+import { getRandomHexCode, isHexCodeValid } from "src/lib/utils.js";
 
 const defaultCurrColoursValue = [
     defaultColourGallery[0].hexCode,
@@ -39,3 +39,49 @@ export const convertWallGensColoursToArrayOfHex = () => {
     const hexCodeList = $derived(wallGenColours.val.map(item => `#${item}`));
     return hexCodeList;
 };
+
+const isColourIndexValid = (index: number) => {
+    return (0 <= index && index <= wallGenColours.val.length - 1);
+};
+
+export const getCurrColourAtIndex = (index: number) => {
+    if (!isColourIndexValid(index)) {
+        throw new Error("ERROR: attempt to retrieve colour with an invalid index");
+        return;
+    }
+
+    return wallGenColours.val[index];
+};
+
+export const setCurrColourAtIndex = (index: number, newValue: string) => {
+    if (!isColourIndexValid(index)) {
+        throw new Error("ERROR: attempt to set colour with an invalid index");
+        return;
+    }
+
+    if (!isHexCodeValid(newValue)) {
+        throw new Error("ERROR: attempt to set colour with an invalid value");
+        return;
+    }
+
+    const tempArr = [...wallGenColours.val];
+    tempArr[index] = newValue;
+    wallGenColours.set(tempArr);
+};
+/**
+ * This pulls the colour at the target index, and adds another random colour to
+ * the end, to maintain the amount of `wallGenColours`.
+ * @param index The target index
+ */
+export const retractCurrColourAtIndex = (index: number) => {
+    const befPortion = wallGenColours.val.slice(0, index);
+    const aftPortion = wallGenColours.val.slice(index + 1);
+    const newRandomisedColour = getRandomHexCode();
+    wallGenColours.set([...befPortion, ...aftPortion, newRandomisedColour]);
+};
+
+// export const getColoursInUse = () => {
+//     const colourCount = getCurrColourInUseCount();
+//     const colourList = $derived(wallGenColours.val.slice(0, colourCount));
+//     return colourList;
+// };
