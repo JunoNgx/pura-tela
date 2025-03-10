9092e457595e65e1c3343cc43760612913e60a60
Juno Nguyen <juno.ngx@gmail.com>
2025-03-11 00:48:57 +0800

    #20: implement themeModeState

---
+++ src/states/themeModeState.svelte.ts
@@ -0,0 +1,19 @@
+import { ThemeMode, type State } from "src/lib/types.js";
+import { createLocalStorageSyncedState } from "./stateUtils.js";
+
+const isThemeModeValid = (data: any) => {
+    if ( data !== ThemeMode.AUTO
+        && data !== ThemeMode.DARK
+        && data !== ThemeMode.LIGHT
+    ) {
+        return false
+    }
+
+    return true;
+};
+
+export const themeMode = createLocalStorageSyncedState({
+    key: "themeMode",
+    defaultValue: ThemeMode.AUTO,
+    validationFunc: isThemeModeValid,
+}) as State<ThemeMode>;
