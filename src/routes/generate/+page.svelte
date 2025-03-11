01ba379cb07b12f56d55c9465c0e02022caab80f
Juno Nguyen <juno.ngx@gmail.com>
2025-03-11 20:54:04 +0800

    #21: move related components to /generate route

--- src/routes/generate/+page.svelte
+++ src/routes/generate/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-	import BasePaletteGenerator from "src/components/BasePaletteGenerator.svelte";
+	import PaletteGenerator from "src/routes/generate/PaletteGenerator.svelte";
 </script>
 
 <h2 class="VisuallyHidden">Palette Generator</h2>
-<BasePaletteGenerator/>
+<PaletteGenerator/>
