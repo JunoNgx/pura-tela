db54f5f455d5afc8fad071689190dedcbf3b1358
Juno Nguyen <juno.ngx@gmail.com>
2025-03-12 22:16:02 +0800

    #20: rename classnames to StyleSelector

--- src/routes/StyleSelector.svelte
+++ src/routes/StyleSelector.svelte
@@ -8,29 +8,29 @@
     }
 </script>
 
-<section class="ModeSelector">
+<section class="StyleSelector">
     <h3>Wallpaper Style</h3>
-    <div class="ModeSelector__Container">
-        <button class="ModeSelector__ModeItem"
-            class:ModeSelector__ModeItem--IsSelected={isSolidMode()}
+    <div class="StyleSelector__Container">
+        <button class="StyleSelector__ModeItem"
+            class:StyleSelector__ModeItem--IsSelected={isSolidMode()}
             onclick={() => { handleWallpaperStyleChange(WallpaperStyle.SOLID); }}
         >
             Solid wallpaper
         </button>
-        <button class="ModeSelector__ModeItem"
-            class:ModeSelector__ModeItem--IsSelected={isGradientMode()}
+        <button class="StyleSelector__ModeItem"
+            class:StyleSelector__ModeItem--IsSelected={isGradientMode()}
             onclick={() => { handleWallpaperStyleChange(WallpaperStyle.GRADIENT); }}
         >
             Gradient wallpaper
         </button>
-        <button class="ModeSelector__ModeItem"
-            class:ModeSelector__ModeItem--IsSelected={isPopArtSquareMode()}
+        <button class="StyleSelector__ModeItem"
+            class:StyleSelector__ModeItem--IsSelected={isPopArtSquareMode()}
             onclick={() => { handleWallpaperStyleChange(WallpaperStyle.POP_ART_SQUARE); }}
         >
             Pop Art Square
         </button>
-        <button class="ModeSelector__ModeItem"
-            class:ModeSelector__ModeItem--IsSelected={isPaletteRowMode()}
+        <button class="StyleSelector__ModeItem"
+            class:StyleSelector__ModeItem--IsSelected={isPaletteRowMode()}
             onclick={() => { handleWallpaperStyleChange(WallpaperStyle.PALETTE_ROW); }}
         >
             Palette Row
@@ -39,14 +39,14 @@
 </section>
 
 <style>
-    .ModeSelector__Container {
+    .StyleSelector__Container {
         display: grid;
         grid-template-columns: repeat(2, 1fr);
         gap: 1rem;
         margin-top: 1rem;
     }
 
-    .ModeSelector__ModeItem--IsSelected {
+    .StyleSelector__ModeItem--IsSelected {
         box-shadow: 0 0 0 4px var(--colPri);
     }
 
