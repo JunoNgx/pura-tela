d5204733af13f6abc1b7b2046cc743132aff4e30
Juno Nguyen <juno.ngx@gmail.com>
2025-03-13 22:33:08 +0800

    #27: pass queries to home route

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
+	import type { WallGenQueryProps } from "src/lib/types.js";
     import Studio from "src/routes/Studio.svelte";
+
+    export let data: WallGenQueryProps;
 </script>
 
 <h2 class="VisuallyHidden">Generate wallpaper</h2>
