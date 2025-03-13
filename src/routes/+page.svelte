fb2f5d964c8b4ca7c6e9b3dd7cb18007d8af8231
Juno Nguyen <juno.ngx@gmail.com>
2025-03-13 23:37:26 +0800

    #27: implement tryParseStyleFromQueryToWallGen()

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,22 @@
 <script lang="ts">
-	import type { WallGenQueryProps } from "src/lib/types.js";
+	import type { WallGenQueryProps, WallpaperStyle } from "src/lib/types.js";
     import Studio from "src/routes/Studio.svelte";
+	import { isWallGenStyleValid, wallGenStyle, } from "src/states/wallGenState.svelte.js";
 
     export let data: WallGenQueryProps;
+
+    const tryParseStyleFromQueryToWallGen = () => {
+        if (!data.style) {
+            return;
+        }
+
+        if (isWallGenStyleValid(data.style)) {
+            wallGenStyle.set(data.style as WallpaperStyle);
+        }
+    };
+
+    tryParseStyleFromQueryToWallGen();
+
 </script>
 
 <h2 class="VisuallyHidden">Generate wallpaper</h2>
