70e3775ba23c1a060187b052fce116340c0366c4
Juno Nguyen <juno.ngx@gmail.com>
2025-03-20 19:54:22 +0800

    #46: remove move items buttons

--- src/routes/WallGenColourInputItem.svelte
+++ src/routes/WallGenColourInputItem.svelte
@@ -2,8 +2,6 @@
     import ColorPicker from 'svelte-awesome-color-picker';
     
     import MaterialSymbolsLightDragIndicator from "~icons/material-symbols-light/drag-indicator";
-    import MaterialSymbolsLightMoveUp from "~icons/material-symbols-light/move-up";
-    import MaterialSymbolsLightMoveDown from "~icons/material-symbols-light/move-down";
     import MaterialSymbolsLightRemove from "~icons/material-symbols-light/remove";
     import MaterialSymbolsLightShuffle from "~icons/material-symbols-light/shuffle";
     import MaterialSymbolsLightSaveOutline from "~icons/material-symbols-light/save-outline";
@@ -44,14 +42,6 @@
         retractWallGenColoursById(colourObj.id);
         decreaseWallGenColourInUseCount();
     };
-
-    // const moveUp = () => {
-    //     moveWallGenColourItemUpAtIndex(index);
-    // };
-
-    // const moveDown = () => {
-    //     moveWallGenColourItemDownAtIndex(index);
-    // };
 </script>
 
 <div class="ColourInput">
@@ -101,22 +91,6 @@
     </div>
 
     <div class="ColourInput__Buttons">
-        <!-- <button class="ColourInput__ColourActionBtn ColourInput__ColourActionBtn--LabelessBtn IconButtonWithLabel"
-            onclick={moveDown}
-            title="Move the colour down"
-            aria-label="Move the colour down"
-            disabled={index >= getWallGenColourInUseCount() - 1}
-        >
-            <MaterialSymbolsLightMoveDown />
-        </button>
-        <button class="ColourInput__ColourActionBtn ColourInput__ColourActionBtn--LabelessBtn IconButtonWithLabel"
-            onclick={moveUp}
-            title="Move the colour up"
-            aria-label="Move the colour up"
-            disabled={index <= 0}
-        >
-            <MaterialSymbolsLightMoveUp />
-        </button> -->
         <button class="ColourInput__ColourActionBtn IconButtonWithLabel"
             onclick={handleRandomise}
             title="Generate a randomised colour"
@@ -189,10 +163,6 @@
         flex-wrap: nowrap;
         gap: 1rem;
     }
-
-    /* .ColourInput__ColourActionBtn--LabelessBtn {
-        padding: 0.5rem;
-    } */
 
     .ColourInput__RemoveBtn {
         color: var(--colDanger);
