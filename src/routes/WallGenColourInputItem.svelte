c45b8868c5813898b9c297e81f3592afa6303682
Juno Nguyen <juno.ngx@gmail.com>
2025-03-20 20:03:40 +0800

    #46: fix incorrect import

--- src/routes/WallGenColourInputItem.svelte
+++ src/routes/WallGenColourInputItem.svelte
@@ -8,7 +8,7 @@
     
 	import { isHexCodeValid, getRandomHexCode } from 'src/lib/utils.js';
 	import { promptAddToColourGallery } from 'src/states/colourGalleryState.svelte.js';
-	import { decreaseWallGenColourInUseCount, getCurrWallStyleInfo, getWallGenColourInUseCount, moveWallGenColourItemDownAtIndex, moveWallGenColourItemUpAtIndex, retractWallGenColoursById, setWallGenColoursAtIndex } from 'src/states/wallGenState.svelte.js';
+	import { decreaseWallGenColourInUseCount, getCurrWallStyleInfo, getWallGenColourInUseCount, retractWallGenColoursById, setWallGenColoursAtIndex } from 'src/states/wallGenState.svelte.js';
 	import type { ColObj } from 'src/lib/types.js';
 	import { dragHandle } from 'svelte-dnd-action';
 
