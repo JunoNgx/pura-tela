fa748d90898cc1313e6b221c6b4ffbb857bb07ec
Juno Nguyen <juno.ngx@gmail.com>
2025-03-22 00:53:46 +0800

    #23: update /help content

--- src/routes/help/+page.svelte
+++ src/routes/help/+page.svelte
@@ -3,42 +3,42 @@
 </script>
 
 <article class="AboutArticle">
-    <h2>Help</h2>
+    <h2 class="H2WithMargin">Help</h2>
 
     <h3>Overview</h3>
     <p>This is <strong>Pura Tela</strong>, a simple web application to play with colours and generate wallpapers from them.</p>
 
     <h3>Wallpaper</h3>
-    <p>The application starts from the <strong>Wallpaper Generator</strong> route, where a wallpaper can be generated from a set of data.</p>
+    <p>The application starts from the <code>Wallpaper Generator</code> route, where a wallpaper can be generated from a set of data.</p>
 
-    <p>The <strong>Wallpaper mode</strong> is the most important setting here. This defines the output of your wallpaper. This is represented in the preview block. You can also enable text samples for to check for the best contrast for your screen.</p>
+    <p>The <code>Wallpaper Style</code> is the most important setting here. This defines the output of your wallpaper. This is represented in the preview block. You can also enable text samples for to check for the best contrast for your screen (which is only for checking, and will not be rendered in the final image).</p>
 
     <h3>Colours</h3>
-    <p>The data input to the wallpaper are made up of colours. Pura Tela is mainly <strong>hex codes</strong>-driven, though an alternative colour picker can be used (represented by a square block). From the <strong>Wallpaper Generator</strong> route, colours can be saved.</p>
+    <p>The data input to the wallpaper are made up of colours. Pura Tela is mainly <strong>hex codes</strong>-driven, though an alternative colour picker can be used (represented by a square coloured block). From the <code>Wallpaper Generator</code> route, colours can be saved.</p>
 
-    <p>A list of saved colours can be accessed from the <strong>Colour Gallery</strong> route. This list can be modified or loaded to the <strong>Wallpaper Generator</strong>.</p>
+    <p>A list of saved colours can be accessed from the <code>Colour Gallery</code> route. This list can be modified or loaded to the <code>Wallpaper Generator</code>.</p>
 
-    <p>Colours can also be removed. This process is permanent and irreversible. At any point of time, to reset the gallery to the default original state, use the "Reset gallery" button at the bottom of the list. In the event that this pre-created curated list changes in the future, this is also how you get access to new items.</p>
+    <p>Colours can also be removed. This process is permanent and irreversible. At any point of time, to reset the gallery to the default original state, use the <code>Reset gallery</code> button at the bottom of the list. In the event that this pre-created curated list changes in the future, this is also how you get access to new items.</p>
 
     <h3>Palettes</h3>
     <p>A combination of colours is called a palette. While colour theory is a complex matter, the application starts with a number of default palette presets you can start using. A palette is limited to have a minimum of {MIN_COLOUR_COUNT_PALETTE} and maximum of {MAX_COLOUR_COUNT} colours.</p>
 
-    <p>Pura Tela comes with a dedicated <strong>Palette Generator</strong> route to assist with the composing process.</p>
+    <p>Pura Tela comes with a dedicated <code>Palette Generator</code> route to assist with the composing process.</p>
 
-    <p>From the <strong>Palette Gallery</strong> route, palettes can be passed to either the <strong>Wallpaper Generator</strong> or the <strong>Palette Generator</strong>. Like the <strong>Colour Gallery</strong>, paletes can also be removed or reset to default.</p>
+    <p>From the <code>Palette Gallery</code> route, palettes can be passed to either the <code>Wallpaper Generator</code> or the <code>Palette Generator</code>. Like the <code>Colour Gallery</code>, paletes can also be removed or reset to default.</p>
 
 
     <h3>Sizes</h3>
 
-    <p>While working with the <strong>Wallpaper Generator</strong>, you may choose the screen size appropriate to your device from the dropdown at the bottom of the panel. The <strong>Size gallery</strong> tab provides a more detail information of each size preset.</p>
+    <p>The output size (width and height) of <code>Wallpaper Generator</code> can be adjusted at the bottom of the panel. While these can be fine tune as you want or need, the <code>Size gallery</code> provides a list of common presets to get you started.</p>
 
-    <p>In the event of doubt, the preset "Mobile" and "Desktop 2K" are applicable to the overwhelming amount of cases.</p>
+    <p>In the event of doubt, the preset <code>Mobile</code> and <code>Desktop 2K</code> are applicable to the overwhelming amount of cases.</p>
 
     <h3>Downloading</h3>
 
-    <p>Once satisfied with the settings, tapping on the <strong>Download</strong> button will create the ouput wallpaper image and should be downloaded to your device. The image file will be in the PNG format and named according to the mode and colours you used.</p>
+    <p>Once satisfied with the settings, tapping on the <code>Download</code> button will create the ouput wallpaper image and should be downloaded to your device. The image file will be in the PNG format and named according to the mode and colours you used.</p>
 
-    <p>The save location varies according to your device configuration, but is typically your designated <strong>Download</strong> directory and should be accessible from your Photo browser app. This image is free for you to keep forever as you see fit, without the need for this app again.</p>
+    <p>The save location varies according to your device configuration, but is typically your designated <code>Download</code> directory and should be accessible from your Photo browser app. This image is free for you to keep forever as you see fit, without the need for this app again.</p>
 
     <h3>Importing palette from external sources</h3>
     <p>From certain screen in the app, it is possible to import colour palettes from an external source as a string. The format is required to follow the following rules:</p>
@@ -60,10 +60,10 @@
     </ul>
 
     <h3>Sharing</h3>
-    <p>To restore the exact preset of settings for the <strong>Wallpaper Generator</strong>, store the url provided at the bottom of the page at a safe place and re-access. You are encourage to do so for either your own personal storage, or sharing with friends.</p>
+    <p>To restore the exact preset of settings for the <code>Wallpaper Generator</code>, store the URL provided at the bottom of the page at a safe place and re-access. You are encourage to do so for either your own personal storage, or sharing with friends.</p>
 
     <h3>Installing</h3>
-    <p>Pura Tela is deployed as a <strong>Progressive Web App</strong>, and is installable to your device to be launched in a manner similar to a native app. If you do not see a prompt, check your browser guide for the appropriate steps.</p>
+    <p>Pura Tela is deployed as a <code>Progressive Web App</code>, and is installable to your device to be launched in a manner similar to a native app. If you do not see a prompt, check your browser guide for the appropriate steps.</p>
 
     <h3>Privacy and tracking</h3>
     <p>Pura Tela does not track your usage and activities in any way. Your changes and states are entirely stored via the web <code>localStorage</code> API. Your data is your data, and nothing is collected more than the hosting platform (Vercel) requires for minimum functioning.</p>
