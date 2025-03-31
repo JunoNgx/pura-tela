4f63823cf3f83a9438d51eec90cf54b46d55a042
Juno Nguyen <juno.ngx@gmail.com>
2025-03-31 23:13:48 +0800

    #53: re-layout StyleConfigColourSwatch

--- src/routes/StyleConfigColourSwatch.svelte
+++ src/routes/StyleConfigColourSwatch.svelte
@@ -35,83 +35,91 @@
         Colour Swatch Configurations
     </h3>
 
-    <fieldset class="ColourSwatchConfig__Item">
-        <legend>
-            <h4 class="ColourSwatchConfig__ItemTitle">
-                Item shape
-            </h4>
-        </legend>
-        <div class="ColourSwatchConfig__ButtonsContainer">
-            <RadioCheckbox
-                value={ColourSwatchStyleItemShape.SQUARE}
-                checked={isShapeSquare}
-                onclick={handleSetItemShape}
-            >
-                Square
-            </RadioCheckbox>
-            <RadioCheckbox
-                value={ColourSwatchStyleItemShape.CIRCLE}
-                checked={isShapeCircle}
-                onclick={handleSetItemShape}
-            >
-                Circle
-            </RadioCheckbox>
-        </div>
-    </fieldset>
+    <div class="ColourSwatchConfig__ItemsContainer">
+        <fieldset class="ColourSwatchConfig__Item">
+            <legend>
+                <h4 class="ColourSwatchConfig__ItemTitle">
+                    Item shape
+                </h4>
+            </legend>
+            <div class="ColourSwatchConfig__ButtonsContainer">
+                <RadioCheckbox
+                    value={ColourSwatchStyleItemShape.SQUARE}
+                    checked={isShapeSquare}
+                    onclick={handleSetItemShape}
+                >
+                    Square
+                </RadioCheckbox>
+                <RadioCheckbox
+                    value={ColourSwatchStyleItemShape.CIRCLE}
+                    checked={isShapeCircle}
+                    onclick={handleSetItemShape}
+                >
+                    Circle
+                </RadioCheckbox>
+            </div>
+        </fieldset>
 
-    <fieldset class="ColourSwatchConfig__Item">
-        <legend>
-            <h4 class="ColourSwatchConfig__ItemTitle">
-                Position
-            </h4>
-        </legend>
-        <div class="ColourSwatchConfig__ButtonsContainer">
-            <RadioCheckbox
-                value={ColourSwatchStylePosition.CENTERED}
-                checked={isPositionCentered}
-                onclick={handleSetPosition}
-            >
-                Centered
-            </RadioCheckbox>
-            <RadioCheckbox
-                value={ColourSwatchStylePosition.TOP_RIGHT}
-                checked={isPositionTopRight}
-                onclick={handleSetPosition}
-            >
-                Top right
-            </RadioCheckbox>
-        </div>
-    </fieldset>
+        <fieldset class="ColourSwatchConfig__Item">
+            <legend>
+                <h4 class="ColourSwatchConfig__ItemTitle">
+                    Position
+                </h4>
+            </legend>
+            <div class="ColourSwatchConfig__ButtonsContainer">
+                <RadioCheckbox
+                    value={ColourSwatchStylePosition.CENTERED}
+                    checked={isPositionCentered}
+                    onclick={handleSetPosition}
+                >
+                    Centered
+                </RadioCheckbox>
+                <RadioCheckbox
+                    value={ColourSwatchStylePosition.TOP_RIGHT}
+                    checked={isPositionTopRight}
+                    onclick={handleSetPosition}
+                >
+                    Top right
+                </RadioCheckbox>
+            </div>
+        </fieldset>
 
-    <fieldset class="ColourSwatchConfig__Item">
-        <legend>
-            <h4 class="ColourSwatchConfig__ItemTitle">
-                Spacing between items
-            </h4>
-        </legend>
-        <div class="ColourSwatchConfig__ButtonsContainer">
-            <RadioCheckbox
-                value={true.toString()}
-                checked={colourSwatchStyleConfig.val.hasSpacing}
-                onclick={handleSetHasSpacing}
-            >
-                Add gap
-            </RadioCheckbox>
-            <RadioCheckbox
-                value={false.toString()}
-                checked={!colourSwatchStyleConfig.val.hasSpacing}
-                onclick={handleSetHasSpacing}
-            >
-                No gap
-            </RadioCheckbox>
-        </div>
-    </fieldset>
+        <fieldset class="ColourSwatchConfig__Item">
+            <legend>
+                <h4 class="ColourSwatchConfig__ItemTitle">
+                    Spacing between items
+                </h4>
+            </legend>
+            <div class="ColourSwatchConfig__ButtonsContainer">
+                <RadioCheckbox
+                    value={true.toString()}
+                    checked={colourSwatchStyleConfig.val.hasSpacing}
+                    onclick={handleSetHasSpacing}
+                >
+                    Add gap
+                </RadioCheckbox>
+                <RadioCheckbox
+                    value={false.toString()}
+                    checked={!colourSwatchStyleConfig.val.hasSpacing}
+                    onclick={handleSetHasSpacing}
+                >
+                    No gap
+                </RadioCheckbox>
+            </div>
+        </fieldset>
+    </div>
 
 </div>
 
 <style>
     .ColourSwatchConfig {
         margin-top: 3rem;
+    }
+
+    .ColourSwatchConfig__ItemsContainer {
+        display: grid;
+        grid-template-columns: repeat(3, 1fr);
+        gap: 1rem;
     }
 
     .ColourSwatchConfig__Title {
@@ -125,5 +133,17 @@
     .ColourSwatchConfig__ButtonsContainer {
         display: flex;
         gap: 2rem;
+    }
+
+    @media screen and (width <= 1000px) {
+        .ColourSwatchConfig__ItemsContainer {
+            grid-template-columns: repeat(2, 1fr);
+        }
+    }
+
+    @media screen and (width <= 500px) {
+        .ColourSwatchConfig__ItemsContainer {
+            grid-template-columns: repeat(1, 1fr);
+        }
     }
 </style>
