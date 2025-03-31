31f8ce2078266ea148588e19e1c6c09f0691291a
Juno Nguyen <juno.ngx@gmail.com>
2025-03-31 21:57:08 +0800

    #53: draft logic for wallGenStyleSettingsColourSwatchState.svelte

---
+++ src/states/wallGenStyleSettingsColourSwatchState.svelte.ts
@@ -0,0 +1,29 @@
+import { ColourSwatchStyleItemShape, ColourSwatchStylePosition, type ColourSwatchStyleSettingsProps, type State } from "src/lib/types.js";
+import { createLocalStorageSyncedState, isEnumValueValid, isValidBoolean } from "./stateUtils.svelte.js"
+
+const isPaletteRowSettingsValid = (data: any) => {
+    if (!data) return false;
+
+    if (!isEnumValueValid(data.itemShape, ColourSwatchStyleItemShape))
+        return false;
+
+    if (!isEnumValueValid(data.position, ColourSwatchStylePosition))
+        return false;
+
+    if (!isValidBoolean(data.hasSpacing))
+        return false;
+
+    return true;
+};
+
+const colourSwatchStyleSettingsDefaultValue = {
+    itemShape: ColourSwatchStyleItemShape.SQUARE,
+    position: ColourSwatchStylePosition.CENTERED,
+    hasSpacing: true,
+};
+
+export const colourSwatchStyleSettings = <State<ColourSwatchStyleSettingsProps>>createLocalStorageSyncedState({
+    key: "paletteRowStyleSettings",
+    defaultValue: colourSwatchStyleSettingsDefaultValue,
+    validationFunc: isPaletteRowSettingsValid
+});
