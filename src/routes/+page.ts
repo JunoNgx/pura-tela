99aaeb566b6192d58391cbf7fc7b9ad288ea0d93
Juno Nguyen <juno.ngx@gmail.com>
2025-04-02 00:49:51 +0800

    #15: retrieve gradient angle from queries

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -9,10 +9,14 @@ export const load = ({
     const width = url.searchParams.get("width");
     const height = url.searchParams.get("height");
 
+    // Gradient config
+    const gradientAngle = url.searchParams.get("gradientAngle");
+
     return {
         style,
         colours,
         width,
         height,
+        gradientAngle,
     };
 };
