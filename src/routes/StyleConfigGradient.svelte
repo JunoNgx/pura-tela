d0ea54a3d2f129b43776abf97cd4cb7d7ea1ee5a
Juno Nguyen <juno.ngx@gmail.com>
2025-04-01 23:16:32 +0800

    #15: implement angle input

--- src/routes/StyleConfigGradient.svelte
+++ src/routes/StyleConfigGradient.svelte
@@ -1,11 +1,22 @@
 <script lang="ts">
 
-	import { gradientStyleConfig } from "src/states/wallGenStyleConfigGradientState.svelte.js";
+	import { gradientStyleConfig, setGradientStyleConfigAngle } from "src/states/wallGenStyleConfigGradientState.svelte.js";
 
     const handleSliderChange = (
         e: Event & { currentTarget: EventTarget & HTMLInputElement; }
     ) => {
+        const newValue = e.currentTarget.value;
 
+        try {
+            const parsedValue = parseInt(newValue);
+            if (parsedValue < 0 || parsedValue > 360)
+                throw new Error("Invalid angle value");
+
+            setGradientStyleConfigAngle(parsedValue);
+        } catch(err) {
+            console.log(err)
+            console.error("Invalid angle value")
+        }
     };
 
 </script>
@@ -20,12 +31,21 @@
 
         <div class="GradientConfig__AngleContentContainer">
 
-            <div class="GradientConfig__CurrValueContainer">
-                <span class="GradientConfig__CurrVal">{gradientStyleConfig.val.angleInDeg}</span>
+            <div class="GradientConfig__AngleInputContainer">
+                <label class="VisuallyHidden" for="gradientAngleInput">Angle input as number input</label>
+                <input class="GradientConfig__AngleInput"
+                    id="gradientAngleInput"
+                    type="number"
+                    min="0"
+                    max="360"
+                    value="{gradientStyleConfig.val.angleInDeg}"
+                    oninput={handleSliderChange}
+                />
+                <span class="GradientConfig__AngleInputUnit">°</span>
             </div>
 
-            <div class="GradientConfig__AngleSlider">
-                <label class="VisuallyHidden" for="gradientAngleSlider">Angle slider input</label>
+            <div class="GradientConfig__AngleSliderContainer">
+                <label class="VisuallyHidden" for="gradientAngleSlider">Angle input as slider</label>
                 <input class="GradientConfig__AngleSliderInput"
                     id="gradientAngleSlider"
                     type="range"
