427f9a1fb2d35ed1b7eae6a42f96e5467f388ff0
Juno Nguyen <juno.ngx@gmail.com>
2025-04-01 22:53:08 +0800

    #15: draft gradient config

---
+++ src/routes/StyleConfigGradient.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+
+</script>
+
+<div class="GradientConfig">
+    <h3 class="GradientConfig__Title">
+        Colour Swatch Configurations
+    </h3>
+
+    <div class="GradientConfig__ItemsContainer">
+        <fieldset class="GradientConfig__Item">
+            <legend>
+                <h4 class="GradientConfig__ItemTitle">
+                    Angle
+                </h4>
+            </legend>
+
+            <div class="GradientConfig__ButtonsContainer">
+
+            </div>
+        </fieldset>
+    </div>
+</div>
+
+<style>
+    .GradientConfig {
+        margin-top: 3rem;
+    }
+
+    .GradientConfig__Title {
+        margin-bottom: 0.5rem;
+    }
+
+    .GradientConfig__Item {
+        border: var(--lineWeight) solid var(--colPri);
+        padding: 0.5rem 1rem 1rem;
+    }
+
+    .GradientConfig__ItemTitle {
+        text-transform: lowercase;
+        margin: 0.5rem 0 0.5rem;
+    }
+
+    .GradientConfig__ButtonsContainer {
+        display: flex;
+        gap: 2rem;
+    }
+</style>
