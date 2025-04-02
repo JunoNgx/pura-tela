0acdcccc4e82c4c8f2d05faf4c78bcbb228b5ba0
Juno Nguyen <juno.ngx@gmail.com>
2025-04-02 21:28:32 +0800

    #57: draft StyleConfigItem

---
+++ src/components/StyleConfigItem.svelte
@@ -0,0 +1,60 @@
+<script lang="ts">
+    type StyleConfigItemProps = {
+        domId: string,
+        label: string,
+        minVal: number,
+        maxVal: number,
+        val: number,
+        step: number,
+        changeHandler: (e: Event & { currentTarget: EventTarget & HTMLInputElement }) => {}
+    };
+
+    let {
+        domId,
+        label,
+        minVal,
+        maxVal,
+        val,
+        step,
+        changeHandler
+    }: StyleConfigItemProps = $props();
+</script>
+
+<div class="StyleConfigItem">
+    <div class="StyleConfigItem__Label">
+        {label}
+    </div>
+
+    <div class="StyleConfigItem__InputContainer">
+        <label class="VisuallyHidden" for={`${domId}-input`}>
+            {`${label} as number input`}
+        </label>
+        <input class="StyleConfigItem__Input"
+            id="{domId}"
+            type="number"
+            min="{minVal}"
+            max="{maxVal}"
+            value="{val}"
+            oninput={changeHandler}
+        />
+    </div>
+
+    <div class="StyleConfigItem__SliderContainer">
+        <label class="VisuallyHidden" for={`${domId}-input`}>
+            {`${label} as slider`}
+        </label>
+        <input class="StyleConfigItem__Slider"
+            id="gradientAngleSlider"
+            type="range"
+            min="{minVal}"
+            max="{maxVal}"
+            step="{step}"
+            value="{val}"
+            oninput={changeHandler}
+        />
+    </div>
+</div>
+
+<style>
+
+</style>
