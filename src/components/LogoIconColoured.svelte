6ca39ce63b2d5397f57657b6820ecd717ed2cd3e
Juno Nguyen <juno.ngx@gmail.com>
2025-04-07 20:17:23 +0800

    #59: update logo colours

--- src/components/LogoIconColoured.svelte
+++ src/components/LogoIconColoured.svelte
@@ -9,17 +9,21 @@
                 stroke-width: 0px;
             }
 
-            .main {
-                fill: #18A0C6;
+            .left {
+                fill: #1AC4F5;
+            }
+
+            .right1 {
+                fill: #22D2D2
             }
 
             .right2 {
-                fill: #d0d0d0;
+                fill: #F5ADAE;
             }
         </style>
     </defs>
 
-    <rect class="item left main" x="40" y="40" width="452" height="944"/>
-    <rect class="item right1 main" x="532" y="40" width="452" height="452"/>
+    <rect class="item left" x="40" y="40" width="452" height="944"/>
+    <rect class="item right1" x="532" y="40" width="452" height="452"/>
     <rect class="item right2" x="532" y="532" width="452" height="452"/>
 </svg>
