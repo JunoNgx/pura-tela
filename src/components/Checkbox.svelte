3b3311de1b7105bb3480554fbb6483b753f4ae27
Juno Nguyen <juno.ngx@gmail.com>
2025-04-22 21:38:32 +0800

    #55: implement reusable Checkbox

---
+++ src/components/Checkbox.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+
+    type CheckboxProps = {
+        className?: string,
+        domId: string,
+        label: string,
+        value: boolean,
+        changeHandler: () => void,
+    };
+
+    let {
+        className,
+        domId,
+        label,
+        value,
+        changeHandler
+    }: CheckboxProps = $props();
+
+</script>
+
+<div class={`${className} Checkbox`}>
+    <input class="Checkbox__Input"
+        id="{domId}"
+        type=checkbox
+        checked={value}
+        onclick={changeHandler}
+    >
+    <label class="Checkbox__Label"
+        for="{domId}"
+    >
+        {label}
+    </label>
+</div>
