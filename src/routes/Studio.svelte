554c1b0b655b49cbbed8210aea6b034876e34d54
Juno Nguyen <juno.ngx@gmail.com>
2025-04-25 21:37:06 +0800

    #61: break out StudioPreview to separate component

--- src/routes/Studio.svelte
+++ src/routes/Studio.svelte
@@ -1,9 +1,8 @@
 <script lang="ts">
 	import { onDestroy, onMount } from "svelte";
-	import { generateImage, renderCanvas, refitCanvasToContainer } from "src/lib/canvas.js";
-	import { computeBaseUrl, computeFilename } from "src/lib/utils.js";
-	import { getHexColourCodesInUse, shouldShowSampleText, wallGenStyle, wallGenSize, readjustWallGenColoursInUseCount, getColourStringsInUse, getStyleConfig } from "src/states/wallGenState.svelte.js";
-	import { colourGallery } from "src/states/colourGalleryState.svelte.js";
+	import { renderCanvas, refitCanvasToContainer } from "src/lib/canvas.js";
+	import { computeBaseUrl } from "src/lib/utils.js";
+	import { getHexColourCodesInUse, wallGenStyle, wallGenSize, readjustWallGenColoursInUseCount, getColourStringsInUse, getStyleConfig } from "src/states/wallGenState.svelte.js";
     
 	import StyleConfigContainer from "src/routes/StyleConfigContainer.svelte";
 	import WallGenColourInputList from "./WallGenColourInputList.svelte";
@@ -12,22 +11,8 @@
 	import { WallpaperStyle } from "src/lib/types.js";
 	import { gradientStyleConfig } from "src/states/wallGenStyleConfigGradientState.svelte.js";
 	import { colourSwatchStyleConfig } from "src/states/wallGenStyleConfigColourSwatchState.svelte.js";
-	import Checkbox from "src/components/Checkbox.svelte";
 	import { paletteStyleConfig } from "src/states/wallGenStyleConfigPaletteState.svelte.js";
-    
-    const handleDownloadClick = () => {
-        const fileName = computeFilename({
-            colours: getColourStringsInUse(),
-            gallery: colourGallery.val,
-            mode: wallGenStyle.val,
-        });
-
-        generateImage(fileName);
-    };
-
-    const handleCheckboxSwitch = () => {
-        shouldShowSampleText.set(!shouldShowSampleText.val);
-    };
+	import StudioPreview from "./StudioPreview.svelte";
 
     const handleResize = () => {
         refitCanvasToContainer();
@@ -87,44 +72,7 @@
         <StyleConfigContainer/>
     </div>
     <div class="Studio__Generator">
-        <div class="Studio__PreviewContainer">
-            <div class="Studio__PreviewBlock"
-                id="CanvasContainer"    
-            >
-                <canvas class="Studio__Canvas"
-                    id="Canvas"
-                ></canvas>
-                <div class="Studio__SampleTextContainer"
-                    class:Studio__SampleTextContainer--IsDisplayed={shouldShowSampleText.val}
-                >
-                    <p class="Studio__SampleText Studio__SampleText--White">
-                        White text
-                    </p>
-                    <p class="Studio__SampleText Studio__SampleText--Black">
-                        Black text
-                    </p>
-                </div>
-            </div>
-
-            <div class="Studio__PreviewSettings">
-                <div class="Studio__SampleTextSetting">
-                    <Checkbox
-                        className="Studio__SampleTextCheckBox"
-                        domId="shouldShowSampleText"
-                        label="Show sample texts"
-                        checked={shouldShowSampleText.val}
-                        changeHandler={handleCheckboxSwitch}
-                    />
-                </div>
-
-                <button class="Studio_DownloadBtn PriBtn"
-                    onclick={handleDownloadClick}
-                    aria-label="Download"
-                >
-                    Download
-                </button>
-            </div>
-        </div>
+        <StudioPreview />
 
         <div class="Studio__Control">
             <div class="Studio__ColourInputContainer">
@@ -160,61 +108,6 @@
         padding: 2rem 0 0;
     }
 
-    .Studio__PreviewContainer {
-        position: relative;
-        width: 100%;
-    }
-
-    .Studio__PreviewBlock,
-    .Studio__SampleTextContainer {
-        aspect-ratio: 3/2;
-    }
-
-    .Studio__PreviewBlock {
-        height: auto;
-        border: var(--lineWeight) solid var(--colPri);
-        box-sizing: border-box;
-        overflow: hidden;
-
-        display: flex;
-        justify-content: space-around;
-        align-items: center;
-    }
-
-    .Studio__SampleTextContainer {
-        width: 100%;
-        position: absolute;
-        top: 0;
-        left: 0;
-        box-sizing: border-box;
-        display: none;
-    }
-
-    .Studio__SampleTextContainer--IsDisplayed {
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        gap: 1rem;
-    }
-
-    .Studio__SampleText--White {
-        color: var(--colWhite);
-    }
-
-    .Studio__SampleText--Black {
-        color: var(--colBlack);
-    }
-
-    .Studio__PreviewSettings {
-        display: flex;
-        flex-direction: row;
-        justify-content: space-between;
-        align-items: center;
-        padding-top: 1rem;
-        /* text-align: center; */
-    }
-
     .Studio__Control {
         flex-grow: 1;
     }
@@ -223,20 +116,10 @@
         margin-top: 3rem;
     }
 
-    .Studio_DownloadBtn {
-        padding: 1rem 2rem;
-        display: block;
-    }
-
     @media screen and (width < 850px) {
         .Studio__Generator {
             padding: 1.5rem 0;
             gap: 4rem;
-        }
-
-        .Studio__PreviewBlock,
-        .Studio__SampleTextContainer {
-            aspect-ratio: 1;
         }
     }
 
---
+++ src/routes/StudioPreview.svelte
@@ -0,0 +1,131 @@
+<script lang="ts">
+	import Checkbox from "src/components/Checkbox.svelte";
+
+	import { generateImage } from "src/lib/canvas.js";
+	import { computeFilename } from "src/lib/utils.js";
+	import { colourGallery } from "src/states/colourGalleryState.svelte.js";
+	import { getColourStringsInUse, shouldShowSampleText, wallGenStyle } from "src/states/wallGenState.svelte.js";
+    
+    const handleDownloadClick = () => {
+        const fileName = computeFilename({
+            colours: getColourStringsInUse(),
+            gallery: colourGallery.val,
+            mode: wallGenStyle.val,
+        });
+
+        generateImage(fileName);
+    };
+
+    const handleCheckboxSwitch = () => {
+        shouldShowSampleText.set(!shouldShowSampleText.val);
+    };
+</script>
+
+<div class="StudioPreview">
+    <div class="StudioPreview__PreviewContent"
+        id="CanvasContainer"    
+    >
+        <canvas class="StudioPreview__Canvas"
+            id="Canvas"
+        ></canvas>
+
+        <div class="StudioPreview__SampleTextContainer"
+            class:StudioPreview__SampleTextContainer--IsDisplayed={shouldShowSampleText.val}
+        >
+            <p class="StudioPreview__SampleText StudioPreview__SampleText--White">
+                White text
+            </p>
+            <p class="StudioPreview__SampleText StudioPreview__SampleText--Black">
+                Black text
+            </p>
+        </div>
+    </div>
+
+    <div class="StudioPreview__Settings">
+        <div class="StudioPreview__SampleTextSetting">
+            <Checkbox
+                className="StudioPreview__SampleTextCheckBox"
+                domId="shouldShowSampleText"
+                label="Show sample texts"
+                checked={shouldShowSampleText.val}
+                changeHandler={handleCheckboxSwitch}
+            />
+        </div>
+
+        <button class="StudioPreview__DownloadBtn PriBtn"
+            onclick={handleDownloadClick}
+            aria-label="Download"
+        >
+            Download
+        </button>
+    </div>
+</div>
+
+<style>
+    .StudioPreview {
+        position: relative;
+        width: 100%;
+    }
+
+    .StudioPreview__PreviewContent,
+    .StudioPreview__SampleTextContainer {
+        aspect-ratio: 3/2;
+    }
+
+    .StudioPreview__PreviewContent {
+        height: auto;
+        border: var(--lineWeight) solid var(--colPri);
+        box-sizing: border-box;
+        overflow: hidden;
+
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+    }
+
+    .StudioPreview__SampleTextContainer {
+        width: 100%;
+        position: absolute;
+        top: 0;
+        left: 0;
+        box-sizing: border-box;
+        display: none;
+    }
+
+    .StudioPreview__SampleTextContainer--IsDisplayed {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        gap: 1rem;
+    }
+
+    .StudioPreview__SampleText--White {
+        color: var(--colWhite);
+    }
+
+    .StudioPreview__SampleText--Black {
+        color: var(--colBlack);
+    }
+
+    .StudioPreview__Settings {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        align-items: center;
+        padding-top: 1rem;
+        /* text-align: center; */
+    }
+
+    .StudioPreview__DownloadBtn {
+        padding: 1rem 2rem;
+        display: block;
+    }
+
+    @media screen and (width < 850px) {
+        .StudioPreview__PreviewContent,
+        .StudioPreview__SampleTextContainer {
+            aspect-ratio: 1;
+        }
+    }
+</style>
