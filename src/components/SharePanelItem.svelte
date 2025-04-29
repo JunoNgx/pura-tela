9415b7c4b5db0851b3e4833bdc941d9951cacae3
Juno Nguyen <juno.ngx@gmail.com>
2025-04-29 22:57:43 +0800

    #73: adjust share panel top alignment

--- src/components/SharePanelItem.svelte
+++ src/components/SharePanelItem.svelte
@@ -119,7 +119,7 @@
         display: flex;
         flex-direction: row;
         justify-content: space-between;
-        align-items: center;
+        align-items: flex-start;
     }
 
     .ShareItem__Label {
