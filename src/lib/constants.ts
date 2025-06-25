b7226fe84f88bd98f1e514daeda03d98857f982f
Juno Nguyen <juno.ngx@gmail.com>
2025-06-25 21:42:57 +0800

    #76: add constant MIN_DIMENSION

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,5 +1,6 @@
 export const MIN_COLOUR_COUNT_PALETTE = 2;
 export const MAX_COLOUR_COUNT = 7;
+export const MIN_DIMENSION = 1;
 export const MAX_WIDTH = 7680;
 export const MAX_HEIGHT = 4320;
 export const HAS_COPY_TIMEOUT_DURATION_MS = 1000;
