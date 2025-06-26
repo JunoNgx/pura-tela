0e6b174fe2a17e1fe5f785f1c41f7aa3577d1f95
Juno Nguyen <juno.ngx@gmail.com>
2025-06-26 22:59:34 +0800

    #79: format /sizes route

--- src/routes/sizes/+page.svelte
+++ src/routes/sizes/+page.svelte
@@ -1,12 +1,16 @@
 <script lang="ts">
-	import { createScrollPositionManager } from "src/lib/scrollPositionManager.js";
+    import { createScrollPositionManager } from "src/lib/scrollPositionManager.js";
 
     import SizeList from "src/routes/sizes/SizeList.svelte";
-	import { sizeGalleryScrollPos } from "src/states/scrollPositionState.svelte.js";
+    import { sizeGalleryScrollPos } from "src/states/scrollPositionState.svelte.js";
 
     createScrollPositionManager("/sizes", sizeGalleryScrollPos);
 </script>
 
 <h2>Size Presets</h2>
-<div class="RouteInstruction">These are preset for common device sizes. Pass them to <code>Wallpaper Generator</code> and fine tune them further there.</div>
-<SizeList/>
+<div class="RouteInstruction">
+    These are preset for common device sizes. Pass them to <code
+        >Wallpaper Generator</code
+    > and fine tune them further there.
+</div>
+<SizeList />
