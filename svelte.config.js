4313dd9cf64fe9c05ffed3b6fdff7cf86970d8fa
Juno Nguyen <juno.ngx@gmail.com>
2025-07-08 23:47:44 +0800

    #3: Fix missing import for child_process

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
 import adapter from "@sveltejs/adapter-auto";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+import * as child_process from 'node:child_process';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
