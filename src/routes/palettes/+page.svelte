c9fca7523998227f9b9c8e6a51f300047d7af2b7
Juno Nguyen <juno.ngx@gmail.com>
2025-10-11 20:57:54 +0800

    #87: implement reload factory palettes button

--- src/routes/palettes/+page.svelte
+++ src/routes/palettes/+page.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
     import MaterialSymbolsDeviceReset from "~icons/material-symbols/device-reset";
+    import MaterialSymbolsResetSettings from "~icons/material-symbols/reset-settings";
 
     import PaletteList from "src/routes/palettes/PaletteList.svelte";
-    import { resetPaletteGallery } from "src/states/paletteGalleryState.svelte.js";
+    import { reloadFactoryPalettes, resetPaletteGallery } from "src/states/paletteGalleryState.svelte.js";
     import { paletteGalleryScrollPos } from "src/states/scrollPositionState.svelte.js";
     import { createScrollPositionManager } from "src/lib/scrollPositionManager.js";
 
@@ -10,12 +11,22 @@
 
     const handleResetGallery = () => {
         const isConfirmed = window.confirm(
-            "Reset the gallery to default colours? This action cannot be undone."
+            "Reset the gallery to default colours? This will also erase your custom data and cannot be undone."
         );
         if (!isConfirmed) return;
 
         resetPaletteGallery();
     };
+
+    const handleReloadFactoryPalettes = () => {
+        const isConfirmed = window.confirm(
+            "Reload the gallery with factory palettes? Your user-created data will remain intact."
+        );
+        if (!isConfirmed) return;
+
+        reloadFactoryPalettes();
+    };
+
 </script>
 
 <h2>Palette Gallery</h2>
@@ -25,19 +36,33 @@
     code.
 </div>
 <PaletteList />
-<button
-    class="ResetButton IconButtonWithLabel"
-    onclick={handleResetGallery}
-    aria-label="Reset colour gallery"
->
-    <MaterialSymbolsDeviceReset />
-    <span>Reset gallery</span>
-</button>
+<div class="ButtonsContainer RightAlignedButtonsContainer">
+    <button
+        class="ReloadFactoryButton IconButtonWithLabel"
+        title="Restore factory pallettes while retaining your custom data"
+        onclick={handleReloadFactoryPalettes}
+        aria-label="Restore factory pallettes while retaining your custom data"
+    >
+        <MaterialSymbolsResetSettings />
+        <span>Reload factory palettes</span>
+    </button>
+    <button
+        class="ResetButton IconButtonWithLabel"
+        title="Delete everything and reset colour gallery to default factory"
+        onclick={handleResetGallery}
+        aria-label="Delete everything and reset colour gallery to default factory"
+    >
+        <MaterialSymbolsDeviceReset />
+        <span>Reset gallery</span>
+    </button>
+</div>
 
 <style>
+    .ReloadFactoryButton {
+        color: var(--colInfo);
+    }
+
     .ResetButton {
         color: var(--colDanger);
-        margin-top: 2rem;
-        margin-left: auto;
     }
 </style>
