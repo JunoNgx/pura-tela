8ea226e77fc746b2b1bf004786cc74fcc1647a9a
Juno Nguyen <juno.ngx@gmail.com>
2026-03-28 10:10:06 +0800

    #92: add palette retrotronic

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -89,12 +89,14 @@
             href="https://colours.cafe/"
             target="_blank"
             rel="noopener noreferrer">colours.cafe</a
-        >
-        and
-        <a
+        >, <a
             href="https://www.behance.net/gallery/32154055/Minimalist-Color-Palettes-2015"
             target="_blank"
             rel="noopener noreferrer">Dumma Branding</a
+        > and <a
+            href="https://lospec.com/palette-list"
+            target="_blank"
+            rel="noopener noreferrer">Lospec.com</a
         > (whom you should check out; they have a lot of beautiful palettes to be
         imported into this app).
     </p>
