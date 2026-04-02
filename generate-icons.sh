954854dbbd474b05447b9c50a9da124da91182ae
Juno Nguyen <juno.ngx@gmail.com>
2026-04-02 21:06:18 +0800

    feat: comment out update for favicon in icon update script

--- generate-icons.sh
+++ generate-icons.sh
@@ -2,7 +2,7 @@
 
 SOURCE="static/icon.png"
 
-convert "$SOURCE" -resize 144x144  static/favicon.png
+# convert "$SOURCE" -resize 144x144  static/favicon.png
 convert "$SOURCE" -resize 180x180  static/icon-180x180.png
 convert "$SOURCE" -resize 256x256  static/icon-256x256.png
 convert "$SOURCE" -resize 512x512  static/icon-512x512.png
