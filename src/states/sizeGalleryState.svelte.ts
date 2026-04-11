61aa2956517ae3d16ee75ef4d58ad8b56b690161
Juno Nguyen <juno.ngx@gmail.com>
2026-04-11 12:17:28 +0800

    #101: chore:  cleanup unnecessary typing in sizeGalleryState

--- src/states/sizeGalleryState.svelte.ts
+++ src/states/sizeGalleryState.svelte.ts
@@ -1,20 +1,8 @@
 import defaultSizeGallery from "src/data/sizes.json" with { type: "json" };
 
-/**
- * Size gallery
- */
-type rawParseSizeOptionItem = {
-    name: string;
-    width: string;
-    height: string;
-};
-
-const generateSizeGallery = (rawOptions: rawParseSizeOptionItem[]) => {
-    return rawOptions.map((sizeOption) => ({
-        name: sizeOption.name,
-        width: parseInt(sizeOption.width),
-        height: parseInt(sizeOption.height),
-    }));
-};
-
-export const sizeGallery = generateSizeGallery(defaultSizeGallery);
+export const sizeGallery = defaultSizeGallery.map((sizeOption) => ({
+    name: sizeOption.name,
+    width: parseInt(sizeOption.width),
+    height: parseInt(sizeOption.height),
+    isFeatured: sizeOption.isFeatured ?? false,
+}));
